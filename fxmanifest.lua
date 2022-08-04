884d20f37ce24f067b47db43ee02b367e62aa0d0
Juan <69590591+TtvNekix@users.noreply.github.com>
2022-08-04 02:59:02 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+shared_scripts {
+    'shared/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
---
+++ shared/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config['TP'] = {
+    ['Zones'] = {
+        {
+            ['x1'] = 227.65,
+            ['y1'] = -872.59,
+            ['z1'] = 30.49,
+            ['name1'] = "test1", -- El nombre no es significativo, no aporta nada al script
+
+            ['x2'] = 158.93,
+            ['y2'] = -995.39,
+            ['z2'] = 29.35,
+            ['name2'] = "test2",
+        }
+    }
+}
