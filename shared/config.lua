a43741f68ac788a51710cb2c18189772bee446e2
Juan <69590591+TtvNekix@users.noreply.github.com>
2022-08-04 04:01:58 +0200

    Add files via upload

---
+++ shared/config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+Config['TP'] = {
+    ['Zones'] = {
+        { -- Example
+            ['x1'] = 227.65,
+            ['y1'] = -872.59,
+            ['z1'] = 30.49,
+            ['label1'] = "Press ~y~E ~w~to go to ~g~TP 2~w~",
+            ['red1'] = 255,
+            ['blue1'] = 0,
+            ['green1'] = 0,
+            ['typeMarker1'] = 1,
+
+            ['x2'] = 158.93,
+            ['y2'] = -995.39,
+            ['z2'] = 29.35,
+            ['label2'] = "Press ~y~E ~w~to go to ~g~TP 1~w~",
+            ['red2'] = 255,
+            ['blue2'] = 0,
+            ['green2'] = 0,
+            ['typeMarker2'] = 1,
+        },
+    }
+}
